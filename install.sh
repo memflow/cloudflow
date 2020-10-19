25803a5e0ae4716f2d9ae487d4826edecf65b1f4
ko1N <ko1N1337@gmail.com>
2020-10-19 19:51:22 +0200

    Fixed install.sh script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ systemctl stop memflow.service
 cp target/release/memflow-cli /usr/bin/memflow
 cp target/release/memflow-daemon /usr/bin/memflowd
 
-mkdir /etc/memflow/
+mkdir -p /etc/memflow/
 cp daemon.conf /etc/memflow/daemon.conf
 
 cp memflow.service /etc/systemd/system/
